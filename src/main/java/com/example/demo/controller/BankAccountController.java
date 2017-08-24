d75a96385eade97f0a83ad31237563260c1eca0c
BrandstadterViktoria <nunviktoria@gmail.com>
2017-08-24 10:30:51 +0200

    Modifie the ExerciseHTML file and the Controller class
    Creates the model attributes in Controller class and adds model attributes to HTMLfile

--- src/main/java/com/example/demo/controller/BankAccountController.java
+++ src/main/java/com/example/demo/controller/BankAccountController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 public class BankAccountController {
-
-
     @RequestMapping("/Exercise1")
     public String showBankAccountDetails(Model model) {
-
+        public String showBankAccountDetails(Model model) {
+            BankAccount lionKing = new BankAccount("Simba", 2000, "lion", true, false);
+            model.addAttribute("name", lionKing.getName());
+            model.addAttribute("balance", lionKing.getBalance());
+            model.addAttribute("animalType", lionKing.getAnimalType());
+        }
         return "ExerciseHTML";
     }
 
