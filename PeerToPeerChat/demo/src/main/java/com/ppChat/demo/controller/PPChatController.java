38e12f1307c5355ac37617a906e1591b922dc6e1
BrandstadterViktoria <nunviktoria@gmail.com>
2017-09-24 17:19:03 +0200

    Modifie mappings in order to examinate user exist in DB

--- PeerToPeerChat/demo/src/main/java/com/ppChat/demo/controller/PPChatController.java
+++ PeerToPeerChat/demo/src/main/java/com/ppChat/demo/controller/PPChatController.java
@@ -4,6 +4,7 @@ import com.ppChat.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PPChatController {
@@ -11,17 +12,31 @@ public class PPChatController {
     private UserRepository userRepository;
 
     @Autowired
-    public PPChatController (UserRepository userRepository) {
+    public PPChatController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        if (userRepository.count() == 0) {
+            return "redirect:/register";
+        } else {
+            return "index";
+        }
     }
 
     @GetMapping("/registration")
-    public String showRegistration (){
+    public String showRegistration () {
+        if(userRepository.count() < 0){
+            return "redirect:/";
+        }
         return "registration";
     }
-}
+
+    @RequestMapping("/registration/enter")
+    public String doRegistration () {
+    return "redirect:/";
+    }
+
+
+    }
