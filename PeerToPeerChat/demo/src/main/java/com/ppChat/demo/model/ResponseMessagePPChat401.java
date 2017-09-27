bcce6e07809d45dcf41949405553b2ce623f0e35
BrandstadterViktoria <nunviktoria@gmail.com>
2017-09-27 09:14:34 +0200

    Create the ResponseMessagePPChat401 model class
    in order to send proper ResponseMessages in case of missing datas in the Input.

---
+++ PeerToPeerChat/demo/src/main/java/com/ppChat/demo/model/ResponseMessagePPChat401.java
@@ -0,0 +1,27 @@
+package com.ppChat.demo.model;
+
+public class ResponseMessagePPChat401 {
+    private String status;
+    private String message;
+
+    public ResponseMessagePPChat401(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
