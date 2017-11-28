e851fd58e7fca358a1aad1680ac6f2cff35a8f08
BrandstadterViktoria <nunviktoria@gmail.com>
2017-11-28 21:32:13 +0100

    Define the class Question for the embedded database

--- QuizApp/demo/src/main/java/com/example/demo/model/Question.java
+++ QuizApp/demo/src/main/java/com/example/demo/model/Question.java
@@ -1,4 +1,37 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Question")
 public class Question {
+
+    @Id
+    @Column
+    private long id;
+    private String question;
+
+
+    public Question() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
 }
